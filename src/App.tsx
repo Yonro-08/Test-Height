658f7f3f329e13c611b6b5e032ece9020ded52cf
Yonro <143833565+Yonro-08@users.noreply.github.com>
2024-07-02 19:21:38 +0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+	const [height, setHeight] = useState(window.innerHeight);
+
+	useEffect(() => {
+		setHeight(window.innerHeight);
+	}, []);
+
+	return (
+		<div className="container">
+			<p>{height}</p>
+			<input
+				type="text"
+				onFocus={() => {
+					setHeight(window.innerHeight);
+				}}
+			/>
+		</div>
+	);
+}
+
+export default App;
