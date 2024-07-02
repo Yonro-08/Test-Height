19a68aeffbbf85eeb717f38fd49d86b13a5ff57f
Yonro <143833565+Yonro-08@users.noreply.github.com>
2024-07-02 19:35:07 +0500

    test scroll 2

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-	const [isInputFocused, setIsInputFocused] = useState(false);
 	const [scrollPosition, setScrollPosition] = useState(0);
 	const [height, setHeight] = useState(window.innerHeight);
 
@@ -21,28 +20,25 @@ function App() {
 		document.body.style.top = `-${window.scrollY}px`;
 		document.body.style.width = '100%';
 		window.addEventListener('touchmove', preventDefault, { passive: false });
-		setIsInputFocused(true);
 	};
 
-	const handleBlur = () => {
-		document.body.style.overflow = '';
-		document.body.style.position = '';
-		document.body.style.top = '';
-		document.body.style.width = '';
-		window.scrollTo(0, scrollPosition);
-		//@ts-ignore
-		window.removeEventListener('touchmove', preventDefault, { passive: false });
-		setIsInputFocused(false);
-	};
+	// const handleBlur = () => {
+	// 	document.body.style.overflow = '';
+	// 	document.body.style.position = '';
+	// 	document.body.style.top = '';
+	// 	document.body.style.width = '';
+	// 	window.scrollTo(0, scrollPosition);
+	// 	//@ts-ignore
+	// 	window.removeEventListener('touchmove', preventDefault, { passive: false });
+	// };
 
 	return (
 		<div className="container">
 			<p>{height}</p>
-			{isInputFocused && <div>Input is focused</div>}
 			<input
 				type="text"
 				onFocus={handleFocus}
-				onBlur={handleBlur}
+				// onBlur={handleBlur}
 				onChange={() => {
 					setHeight(window.innerHeight);
 				}}
